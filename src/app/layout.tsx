f66da92a82dd0cd997bf6a0443461c226848bfdd
r0undy <salesrhandie@gmail.com>
2026-03-07 23:42:39 +0800

    feat: update metadata and image references for CHAINSEC event

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,26 +11,26 @@ const googleSans = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "GDG Photobooth | Santa Doesn’t Know U Like I Do: A Design Jam",
-  description: "Capture amazing photos at the Santa Doesn’t Know U Like I Do: A Design Jam!",
+  title: "GDG Photobooth | CHAINSEC: A Blockchain Security Fundamentals Jam",
+  description: "Capture amazing photos at CHAINSEC: A Blockchain Security Fundamentals Jam by GDG on Campus PUP!",
   openGraph: {
-    title: "GDG Photobooth | Santa Doesn’t Know U Like I Do: A Design Jam",
-    description: "Capture amazing photos at the Santa Doesn’t Know U Like I Do: A Design Jam!",
+    title: "GDG Photobooth | CHAINSEC: A Blockchain Security Fundamentals Jam",
+    description: "Capture amazing photos at CHAINSEC: A Blockchain Security Fundamentals Jam by GDG on Campus PUP!",
     images: [
       {
-        url: "/sharing.webp",
+        url: "/sharing.jpg",
         width: 1200,
         height: 630,
-        alt: "GDG Photobooth | Santa Doesn’t Know U Like I Do: A Design Jam",
+        alt: "GDG Photobooth | CHAINSEC: A Blockchain Security Fundamentals Jam",
       },
     ],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "GDG Photobooth | Santa Doesn’t Know U Like I Do: A Design Jam",
-    description: "Capture amazing photos at the Santa Doesn’t Know U Like I Do: A Design Jam!",
-    images: ["/sharing.webp"],
+    title: "GDG Photobooth | CHAINSEC: A Blockchain Security Fundamentals Jam",
+    images: ["/sharing.jpg"],
+    description: "Capture amazing photos at CHAINSEC: A Blockchain Security Fundamentals Jam by GDG on Campus PUP!",
   },
 };
 
